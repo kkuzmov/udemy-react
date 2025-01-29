03b8944ae57b5055e8f00988b6eb3bfda8e9674f
Krum Kuzmov <kkuzmov@gmail.com>
2025-01-29 19:59:05 +0200

    setup basic components

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -2,8 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
+function Header() {
+  return <h1>Nuts 'N Bolts </h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Please, inspect our catalog</h2>
+      <Nut />
+      <Nut />
+      <Nut />
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <footer>
+      <p>We're here to hizzelp.</p>
+    </footer>
+  );
+  // return React.createElement("footer", null, "We're here to help");
+}
+function Nut() {
+  return <h2>NUTTIN</h2>;
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
