e1171252f5a8ca7fa8f4b21e482c487cabd085d6
Krum Kuzmov <kkuzmov@gmail.com>
2025-01-30 23:02:53 +0200

    remove images; add styles;

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -2,6 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const hardwareData = [
+  {
+    name: "Flat Washer",
+    description: "Stainless steel with zinc coating and smooth finish",
+    price: 6,
+    photoSrc: "images/washer.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Hex Bolt",
+    description: "Carbon steel and zinc plating",
+    price: 10,
+    photoSrc: "images/hexbolt.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Lock Nut",
+    description: "Steel alloy, nylon insert, and serrated flange",
+    price: 12,
+    soldOut: false,
+  },
+  {
+    name: "Wing Nut",
+    description: "Brass body with butterfly wings and threading",
+    price: 12,
+    photoSrc: "images/wingnut.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Carriage Bolt",
+    description: "Steel with square neck and round head",
+    price: 15,
+    photoSrc: "images/carriagebolt.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Machine Screw",
+    description: "Stainless steel, Phillips head, and fine threads",
+    price: 18,
+    soldOut: false,
+  },
+];
+
 function App() {
   return (
     <div className="container">
@@ -21,13 +64,21 @@ function Header() {
 }
 
 function Menu() {
+  const allHardware = hardwareData;
+
   return (
-    <div className="menu">
-      <h2>Catalog</h2>
-      <Nut name="5mm" material="Steel" price="0.05" />
-      <Nut name="15mm" material="Magnesium" price="62" />
-      <Nut name="0.255mm" material="Diamant" price="632" />
-    </div>
+    <ul className="nuts">
+      {allHardware.map((nut) => {
+        return (
+          <Nut
+            name={nut.name}
+            description={nut.description}
+            price={nut.price}
+            src={nut.photoSrc ? nut.photoSrc : "images/no-image.jpg"}
+          />
+        );
+      })}
+    </ul>
   );
 }
 function Footer() {
@@ -40,12 +91,15 @@ function Footer() {
 }
 function Nut(props) {
   return (
-    <div className="pizza">
-      <p>
-        {props.material} {props.name} Nut
-      </p>
-      <p>{props.price} $</p>
-    </div>
+    <li className="nut">
+      <h3 className="nut__name">{props.name}</h3>
+      <p className="nut__description">{props.description}</p>
+      <img
+        src={props.src ? props.src : "images/no-image.png"}
+        alt={props.name}
+      />
+      <p className="nut__price">{props.price}$</p>
+    </li>
   );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
