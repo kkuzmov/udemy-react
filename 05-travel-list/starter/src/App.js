454366083ffa93c131b3e3708776cc7ccdaca7ad
Krum Kuzmov <kkuzmov@gmail.com>
2025-02-03 23:32:31 +0200

    form

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,10 +21,66 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [count, setCount] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, count, packed: false, id: Date.now() };
+    setDescription("");
+    setCount(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you want to bring with you?</h3>
-    </div>
+      <select
+        onChange={(e) => {
+          setCount(Number(e.target.value));
+        }}
+        value={count}
+      >
+        <option value={1} key={1}>
+          1
+        </option>
+        <option value={2} key={2}>
+          2
+        </option>
+        <option value={3} key={3}>
+          3
+        </option>
+        <option value={4} key={4}>
+          4
+        </option>
+        <option value={5} key={5}>
+          5
+        </option>
+        <option value={6} key={6}>
+          6
+        </option>
+        <option value={7} key={7}>
+          7
+        </option>
+        <option value={8} key={8}>
+          8
+        </option>
+        <option value={9} key={9}>
+          9
+        </option>
+        <option value={10} key={10}>
+          10
+        </option>
+      </select>
+      <input
+        type="text"
+        placeholder="Enter an item to take.."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add item</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +88,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -43,7 +101,6 @@ function Stats() {
     </footer>
   );
 }
-
 function Item({ item }) {
   return (
     <li>
