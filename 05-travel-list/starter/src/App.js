291b8f59d82dab02d19a418e7d3076ed05605142
Krum Kuzmov <kkuzmov@gmail.com>
2025-02-03 22:56:39 +0200

    add basic components

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,7 +1,32 @@
 export default function App() {
-  return <Logo />;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 function Logo() {
-  return <p>Logo.</p>;
+  return <h1>🌴 Your Horizon 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you want to bring with you?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list and you packed X of them (X%)</em>
+    </footer>
+  );
 }
