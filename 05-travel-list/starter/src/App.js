27a6faec894d6421c6da4303ddd541371513a898
Krum Kuzmov <kkuzmov@gmail.com>
2025-02-08 10:17:24 +0200

    toggle item

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -9,19 +9,20 @@ export default function App() {
   }
 
   function handleDeleteItem(id) {
-    console.log(id);
-    console.log(items);
-
     setItems((items)=> items.filter((item)=> item.id !== id)
     );
-    console.log(items);
-
   }
+
+  function handleToggleItem(id) {
+    setItems((items)=> items.map((item)=> item.id == id ? {...item, packed : !item.packed} : item)
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItem}/>
-      <PackingList items={items} onDeleteItem={handleDeleteItem}/>
+      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
       <Stats />
     </div>
   );
@@ -98,12 +99,12 @@ function Form({onAddItems}) {
     </form>
   );
 }
-function PackingList({items, onDeleteItem}) {
+function PackingList({items, onDeleteItem, onToggleItem}) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem}/>
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
         ))}
       </ul>
     </div>
@@ -116,9 +117,10 @@ function Stats() {
     </footer>
   );
 }
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input type="checkbox" value={item.packed} onChange={()=>{onToggleItem(item.id)}}/>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description} <button onClick={()=>{onDeleteItem(item.id)}}>❌</button>
       </span>
