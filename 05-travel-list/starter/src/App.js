ad91c81857cf26fe273a8f0eac8b81c0d5a3754b
Krum Kuzmov <kkuzmov@gmail.com>
2025-02-08 10:37:29 +0200

    sortby function

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -99,6 +99,8 @@ function Form({onAddItems}) {
   );
 }
 function PackingList({items, onDeleteItem, onToggleItem}) {
+  const [sortBy, setSortBy] = useState('input');
+  
   return (
     <div className="list">
       <ul>
@@ -106,6 +108,15 @@ function PackingList({items, onDeleteItem, onToggleItem}) {
           <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
         ))}
       </ul>
+      <div className="actions">
+      <select value={sortBy} onChange={(e)=>{
+        setSortBy(e.target.value)
+      }}>
+        <option value="input">Sort by order of input</option>
+        <option value="description">Sort by description</option>
+        <option value="packed">Sort by packed status</option>
+      </select>
+      </div>
     </div>
   );
 }
