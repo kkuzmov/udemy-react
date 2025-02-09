ee8d917f331ba378213d1a7f51015d3371dff866
Krum Kuzmov <kkuzmov@gmail.com>
2025-02-09 11:44:34 +0200

    add friends component

--- 06-eat-n-split/starter/src/App.js
+++ 06-eat-n-split/starter/src/App.js
@@ -34,7 +34,20 @@ function FriendsList(){
   return (
     <ul>
       {initialFriends.map((friend) => 
-        <li>{friend.name}</li>)}
+        <Friend friend={friend} key={friend.id}/>)}
     </ul>
   )
+}
+
+function Friend({friend}) {
+  
+  return (
+  <li>
+    <img src={friend.image} alt={friend.name}/>
+    <h3>{friend.name}</h3>
+    {friend.balance < 0 && (<p className="red">You owe {friend.name} ${Math.abs(friend.balance)}</p>)}
+    {friend.balance === 0 && (<p>You are equal with {friend.name}</p>)}
+    {friend.balance > 0 && (<p className="green">{friend.name} owes you ${Math.abs(friend.balance)}</p>)}
+    <button className="button">Select</button>
+    </li>)
 }
